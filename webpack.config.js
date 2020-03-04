91afb027189ee8bcaa92cc9f0eec7afd9410f249
PetrDusek <petr.dusek@brain-station.com>
2020-03-04 15:30:35 +0100

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,22 @@
-
-const webpack = require('webpack');
-const path = require('path');
-const NpmPackPlugin = require('@signageos/lib/dist/Webpack/NpmPackPlugin').default;
-const parameters = require('./config/parameters');
-
-const packageConfigPath = `${parameters.paths.rootPath}/package.json`;
+const { CheckerPlugin } = require('awesome-typescript-loader');
 
 module.exports = {
-    entry: ['@babel/polyfill', './src/index'],
+    entry: './src/index.ts',
     output: {
-        path: parameters.paths.distPath,
-        filename: 'bundle.js',
-        publicPath: '/',
-        libraryTarget: 'umd',
+        filename: 'index.js',
     },
-    devtool: 'source-map',
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': `"${parameters.environment}"`,
-        }),
-        new webpack.WatchIgnorePlugin([path.resolve(packageConfigPath)]),
-        new NpmPackPlugin({
-            name: parameters.app.name,
-            environment: parameters.environment,
-            rootPath: parameters.paths.rootPath,
-            packagesPath: parameters.paths.packagesPath,
-        }),
-    ],
     resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.json'],
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
     },
     module: {
         rules: [
-            { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
             {
-                test: /\.(t|j)sx?$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                options: { presets: [require.resolve('@babel/preset-env')] },
-                enforce: 'post',
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader',
             },
         ],
     },
-}
+    plugins: [
+        new CheckerPlugin(),
+    ],
+};
