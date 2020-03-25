0ef76240c9e39ee256bbfbeff35840730541bb67
PetrDusek <petr.dusek@brain-station.com>
2020-03-25 16:42:09 +0100

    add files tools tests

---
+++ test/unit/tools/files.spec.ts
@@ -0,0 +1,34 @@
+import * as chai from 'chai';
+import { getFileName } from "../../../src/components/files/tools";
+
+const expect = chai.expect;
+
+describe('Files tools component', () => {
+
+	describe('Files tools component getFileName tests', () => {
+		it('Should return correct file name for vairous strings', () => {
+			const filesPaths = [
+				'https://cors-anywhere.herokuapp.com/https://butikstv.centrumkanalen.com/play/smil/234.smil',
+				'http://butikstv.centrumkanalen.com/play/media/rendered/bilder/10826.png',
+				'localFile/inFolder/something//myfile.txt',
+				'../file.png',
+				'./../../../idontknow.mp3',
+				'fileName.mp4',
+			];
+			const fileNames = [
+				'234.smil',
+				'10826.png',
+				'myfile.txt',
+				'file.png',
+				'idontknow.mp3',
+				'fileName.mp4',
+			];
+
+			for (let i = 0; i < filesPaths.length; i+= 1) {
+				const response = getFileName(filesPaths[i]);
+				expect(response).to.be.equal(fileNames[i]);
+			}
+		});
+
+	});
+});
