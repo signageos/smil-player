e940ba16157e63d3c4f6d02818b0145d97817a0f
Michael Zabka <zabka.michael@gmail.com>
2020-04-06 16:00:40 +0200

    Add CI pipeline for gitlab

---
+++ tools/prepare-npm-public.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const packageConfig = require("../package");
+
+fs.writeFileSync(
+	"./package.json",
+	JSON.stringify(
+		{
+			...packageConfig,
+			publishConfig: {
+				registry: "https://registry.npmjs.org/",
+				access: "public",
+			},
+			files: [
+				'tools',
+				'package.json',
+			],
+		},
+		undefined,
+		2,
+	) + "\n",
+);
