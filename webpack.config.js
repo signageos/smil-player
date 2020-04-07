4bb972ee1b19ae889d3e4449500d554f20d579ce
Michael Zabka <zabka.michael@gmail.com>
2020-04-07 18:39:26 +0200

    Remove default CORS_ANYWHERE
    * it is usable only for dev in chrome emulator so it could be set for dev only
    * run cors-anywhere server for dev builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,44 @@
+const webpack = require('webpack');
 const { CheckerPlugin } = require('awesome-typescript-loader');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const SignageOSPlugin = require('@signageos/cli/dist/Webpack/Plugin');
 
-module.exports = {
-    target: 'web',
-    entry: './src/index.ts',
-    output: {
-        filename: 'index.js',
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: 'awesome-typescript-loader',
-            },
+module.exports = (_env, argv) => {
+
+    if (argv.mode === 'development' && argv.serveIndex) {
+        // webpack-dev-server
+        require('./tools/cors-anywhere');
+    }
+
+    return {
+        target: 'web',
+        entry: './src/index.ts',
+        output: {
+            filename: 'index.js',
+        },
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js', '.jsx'],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: 'awesome-typescript-loader',
+                },
+            ],
+        },
+        plugins: [
+            new webpack.EnvironmentPlugin({
+                ...argv.mode === 'development' ? {
+                    CORS_ANYWHERE: 'http://localhost:8086/',
+                } : {},
+            }),
+            new CheckerPlugin(),
+            new HtmlWebpackPlugin({
+                template: 'public/index.html',
+                inlineSource: '.(js|css)$', // embed all javascript and css inline
+            }),
+            new SignageOSPlugin()
         ],
-    },
-    plugins: [
-        new CheckerPlugin(),
-        new HtmlWebpackPlugin({
-            template: 'public/index.html',
-            inlineSource: '.(js|css)$', // embed all javascript and css inline
-        }),
-        new SignageOSPlugin()
-    ],
+    };
 };
