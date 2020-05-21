6ab59f2cf168d950aaa932c7f7e590fea2f582cb
PetrDusek <petr.dusek@brain-station.com>
2020-05-21 09:22:17 +0200

    add relative filepaths handling (media/file.mp4)

--- src/components/files/tools/index.ts
+++ src/components/files/tools/index.ts
@@ -4,3 +4,11 @@ export const debug = Debug('@signageos/smil-player:filesModule');
 export function getFileName(filePath: string) {
 	return filePath.substring(filePath.lastIndexOf('/') + 1);
 }
+
+export function getPath(filePath: string) {
+	return filePath.substring(0, filePath.lastIndexOf('/') + 1);
+}
+
+export function isLocalUrl(filePath: string) {
+	return (filePath.indexOf('/') > 0 && filePath.indexOf('.') > 0);
+}
