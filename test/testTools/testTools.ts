8c40c9a16b395f2e72aada58d85ae5607581cbc5
PetrDusek <petr.dusek@brain-station.com>
2020-06-02 11:26:08 +0200

    create testTools for date formatting

---
+++ test/testTools/testTools.ts
@@ -0,0 +1,11 @@
+import moment from 'moment';
+
+export function formatDate(date: moment.Moment): string {
+	return date.format().split('.')[0].split('+')[0];
+}
+
+// combine string wallclock(R/2011-01-01T07:00:00/P1D) and +w3 to wallclock(R/2011-01-01+w3T07:00:00/P1D)
+export function formatWeekDate(dateString: string, weekDay: string): string {
+	const strArray = dateString.split('T');
+	return `${strArray[0]}${weekDay}T${strArray[1]}`;
+}
