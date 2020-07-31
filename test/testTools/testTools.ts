9df4cdc03d0a41031b6562c85f902f88cf1d9230
PetrDusek <petr.dusek@brain-station.com>
2020-07-31 10:31:21 +0200

    update tests, remove unused code

--- test/testTools/testTools.ts
+++ test/testTools/testTools.ts
@@ -1,7 +1,7 @@
 import moment from 'moment';
 
 export function formatDate(date: moment.Moment): string {
-	return date.format().split('.')[0].split('+')[0];
+	return date.format('YYYY-MM-DDTHH:mm:ss');
 }
 
 // combine string wallclock(R/2011-01-01T07:00:00/P1D) and +w3 to wallclock(R/2011-01-01+w3T07:00:00/P1D)
@@ -10,7 +10,7 @@ export function formatWeekDate(dateString: string, weekDay: string): string {
 	return `${strArray[0]}${weekDay}T${strArray[1]}`;
 }
 
-export function computeWaitInteral(weekToday: number, weekScheduled: number): number {
+export function computeWaitInterval(weekToday: number, weekScheduled: number): number {
 	if (weekToday <= weekScheduled) {
 		// 24 hours in ms
 		return ((weekScheduled - weekToday) * 86400000);
