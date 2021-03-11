8f7a7dd3530cded680ec8a10e55da6a434b545bd
PetrDusek <petr.dusek@signageos.io>
2021-03-11 17:47:56 +0100

    fix tests

--- cypress/integration/e2e/priorityStop.spec.ts
+++ cypress/integration/e2e/priorityStop.spec.ts
@@ -43,7 +43,16 @@ describe("priorityStop.smil test", () => {
 		cy.get('video[src*="videos/video-test-2_e2ffa51f6a4473b815f39e7fb39239da.mp4"]', { timeout: CypressTimeouts.elementAwaitTimeout }).should('not.be.visible');
 		testCoordinates(cy.iframe().find('img[src*="images/img_3.jpg"]'), 0, 0, 1280, 720);
 
+		cy.iframe().find('img[src*="images/img_3.jpg"]', { timeout: CypressTimeouts.elementAwaitTimeout }).should('not.be.visible');
+		cy.get('video[src*="videos/video-test-2_e2ffa51f6a4473b815f39e7fb39239da.mp4"]', { timeout: CypressTimeouts.elementAwaitTimeout }).should('be.visible');
+		testCoordinates(cy.get('video[src*="videos/video-test-2_e2ffa51f6a4473b815f39e7fb39239da.mp4"]'), 0, 0, 1280, 720);
+		cy.wait(CypressTimeouts.videoTransitionTimeout);
+
+		cy.get('video[src*="videos/video-test-2_e2ffa51f6a4473b815f39e7fb39239da.mp4"]', { timeout: CypressTimeouts.elementAwaitTimeout }).should('be.visible');
+		testCoordinates(cy.get('video[src*="videos/video-test-2_e2ffa51f6a4473b815f39e7fb39239da.mp4"]'), 0, 0, 1280, 720);
+
 		cy.get('video[src*="videos/video-test-1_e07fc21a7a72e3d33478243bd75d7743.mp4"]', { timeout: CypressTimeouts.elementAwaitTimeout }).should('be.visible');
+		cy.iframe().find('img[src*="images/img_3.jpg"]', { timeout: CypressTimeouts.elementAwaitTimeout }).should('not.be.visible');
 		testCoordinates(cy.get('video[src*="videos/video-test-1_e07fc21a7a72e3d33478243bd75d7743.mp4"]'), 0, 0, 1280, 720);
 
 		cy.get('video[src*="videos/video-test-1_e07fc21a7a72e3d33478243bd75d7743.mp4"]', { timeout: CypressTimeouts.elementAwaitTimeout }).should('not.be.visible');
@@ -62,9 +71,9 @@ describe("priorityStop.smil test", () => {
 		cy.iframe().find('img[src*="images/img_1.jpg"]', { timeout: CypressTimeouts.elementAwaitTimeout }).should('not.be.visible');
 		testCoordinates(cy.get('video[src*="videos/video-test-1_e07fc21a7a72e3d33478243bd75d7743.mp4"]'), 0, 0, 1280, 720);
 
+		cy.get('video[src*="videos/video-test-2_e2ffa51f6a4473b815f39e7fb39239da.mp4"]', { timeout: CypressTimeouts.elementAwaitTimeout }).should('be.visible');
 		cy.get('video[src*="videos/video-test-1_e07fc21a7a72e3d33478243bd75d7743.mp4"]', { timeout: CypressTimeouts.elementAwaitTimeout }).should('not.be.visible');
-		cy.iframe().find('img[src*="images/img_1.jpg"]', { timeout: CypressTimeouts.elementAwaitTimeout }).should('be.visible');
-		testCoordinates(cy.iframe().find('img[src*="images/img_1.jpg"]'), 0, 0, 1280, 720);
+		testCoordinates(cy.get('video[src*="videos/video-test-2_e2ffa51f6a4473b815f39e7fb39239da.mp4"]'), 0, 0, 1280, 720);
 
 		cy.iframe().find('img[src*="images/img_3.jpg"]', { timeout: CypressTimeouts.elementAwaitTimeout }).should('be.visible');
 		cy.iframe().find('img[src*="images/img_1.jpg"]', { timeout: CypressTimeouts.elementAwaitTimeout }).should('not.be.visible');
@@ -72,6 +81,7 @@ describe("priorityStop.smil test", () => {
 
 		cy.iframe().find('img[src*="images/img_3.jpg"]', { timeout: CypressTimeouts.elementAwaitTimeout }).should('not.be.visible');
 		cy.get('video[src*="videos/video-test-2_e2ffa51f6a4473b815f39e7fb39239da.mp4"]', { timeout: CypressTimeouts.elementAwaitTimeout }).should('be.visible');
+		cy.iframe().find('img[src*="images/img_1.jpg"]', { timeout: CypressTimeouts.elementAwaitTimeout }).should('not.be.visible');
 		testCoordinates(cy.get('video[src*="videos/video-test-2_e2ffa51f6a4473b815f39e7fb39239da.mp4"]'), 0, 0, 1280, 720);
 		cy.wait(CypressTimeouts.videoTransitionTimeout);
 
@@ -82,24 +92,17 @@ describe("priorityStop.smil test", () => {
 		cy.get('video[src*="videos/video-test-2_e2ffa51f6a4473b815f39e7fb39239da.mp4"]', { timeout: CypressTimeouts.elementAwaitTimeout }).should('not.be.visible');
 		testCoordinates(cy.iframe().find('img[src*="images/img_3.jpg"]'), 0, 0, 1280, 720);
 
-		cy.iframe().find('img[src*="images/img_3.jpg"]', { timeout: CypressTimeouts.elementAwaitTimeout }).should('not.be.visible');
-		cy.get('video[src*="videos/video-test-2_e2ffa51f6a4473b815f39e7fb39239da.mp4"]', { timeout: CypressTimeouts.elementAwaitTimeout }).should('be.visible');
-		testCoordinates(cy.get('video[src*="videos/video-test-2_e2ffa51f6a4473b815f39e7fb39239da.mp4"]'), 0, 0, 1280, 720);
-		cy.wait(CypressTimeouts.videoTransitionTimeout);
-
-		cy.get('video[src*="videos/video-test-2_e2ffa51f6a4473b815f39e7fb39239da.mp4"]', { timeout: CypressTimeouts.elementAwaitTimeout }).should('be.visible');
-		testCoordinates(cy.get('video[src*="videos/video-test-2_e2ffa51f6a4473b815f39e7fb39239da.mp4"]'), 0, 0, 1280, 720);
+		cy.iframe().find('img[src*="images/img_1.jpg"]', { timeout: CypressTimeouts.elementAwaitTimeout }).should('not.be.visible');
+		cy.get('video[src*="videos/video-test-1_e07fc21a7a72e3d33478243bd75d7743.mp4"]', { timeout: CypressTimeouts.elementAwaitTimeout }).should('be.visible');
+		testCoordinates(cy.get('video[src*="videos/video-test-1_e07fc21a7a72e3d33478243bd75d7743.mp4"]'), 0, 0, 1280, 720);
 
 		cy.iframe().find('img[src*="images/img_1.jpg"]', { timeout: CypressTimeouts.elementAwaitTimeout }).should('be.visible');
-		cy.get('video[src*="videos/video-test-2_e2ffa51f6a4473b815f39e7fb39239da.mp4"]', { timeout: CypressTimeouts.elementAwaitTimeout }).should('not.be.visible');
+		cy.get('video[src*="videos/video-test-1_e07fc21a7a72e3d33478243bd75d7743.mp4"]', { timeout: CypressTimeouts.elementAwaitTimeout }).should('not.be.visible');
 		testCoordinates(cy.iframe().find('img[src*="images/img_1.jpg"]'), 0, 0, 1280, 720);
 
 		cy.iframe().find('img[src*="images/img_1.jpg"]', { timeout: CypressTimeouts.elementAwaitTimeout }).should('not.be.visible');
 		cy.iframe().find('img[src*="images/img_2.jpg"]', { timeout: CypressTimeouts.elementAwaitTimeout }).should('be.visible');
 		testCoordinates(cy.iframe().find('img[src*="images/img_2.jpg"]'), 0, 0, 1280, 720);
 
-		cy.get('video[src*="videos/video-test-1_e07fc21a7a72e3d33478243bd75d7743.mp4"]', { timeout: CypressTimeouts.elementAwaitTimeout }).should('be.visible');
-		cy.iframe().find('img[src*="images/img_2.jpg"]', { timeout: CypressTimeouts.elementAwaitTimeout }).should('not.be.visible');
-		testCoordinates(cy.get('video[src*="videos/video-test-1_e07fc21a7a72e3d33478243bd75d7743.mp4"]'), 0, 0, 1280, 720);
 	});
 });
