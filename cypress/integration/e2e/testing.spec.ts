082d70a3f8b0ce7818ff4cefdfec9362603b55bf
PetrDusek <petr.dusek@signageos.io>
2021-03-25 15:42:48 +0100

    update cypress tests

--- cypress/integration/e2e/testing.spec.ts
+++ cypress/integration/e2e/testing.spec.ts
@@ -1,13 +1,13 @@
-// import { testCoordinates } from '../../tools/tools';
-// import { CypressTimeouts, SMILUrls } from '../../enums/enums';
-//
-// describe("wallclockNoActiveSeq.smil test", () => {
-// 	it("processes smil file correctly", () => {
-// 		cy.visit("/");
-// 		cy.frameLoaded('iframe');
-// 		cy.iframe().find('#SMILUrl').clear().type('http://localhost:3000/testing.smil');
-// 		cy.wait(CypressTimeouts.submitTimeout);
-// 		cy.iframe().find('#SMILUrlWrapper').submit();
-//
-// 	});
-// });
+import { testCoordinates } from '../../tools/tools';
+import { CypressTimeouts, SMILUrls } from '../../enums/enums';
+
+describe("wallclockNoActiveSeq.smil test", () => {
+	it("processes smil file correctly", () => {
+		cy.visit("/");
+		cy.frameLoaded('iframe');
+		cy.iframe().find('#SMILUrl').clear().type('http://localhost:3000/testing.smil');
+		cy.wait(CypressTimeouts.submitTimeout);
+		cy.iframe().find('#SMILUrlWrapper').submit();
+
+	});
+});
