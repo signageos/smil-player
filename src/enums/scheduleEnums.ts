8ad05e9cb86136c022d2a256f29526033c2da20b
PetrDusek <petr.dusek@signageos.io>
2021-10-12 17:40:47 +0200

    varios wallclock bugs test

--- src/enums/scheduleEnums.ts
+++ src/enums/scheduleEnums.ts
@@ -1,5 +1,5 @@
 export enum SMILScheduleEnum {
-	endDateAndTimeFuture = 'wallclock(R/2100-01-01T00:00:00/P1D)',
+	endDateAndTimeFuture = 'wallclock(2100-01-01T23:59:59)',
 	endDateAndTimePast = '1970-01-01T00:00:00',
 	endDatePast = '1970-01-01',
 	neverPlay = -3600000,
