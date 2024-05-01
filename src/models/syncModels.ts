214301d4c3df9f8cfb7d3f996b68eded8fc4964a
PetrDusek <petr.dusek@signageos.io>
2024-05-01 17:27:10 +0200

    fix skipping content to sync devices with priority

--- src/models/syncModels.ts
+++ src/models/syncModels.ts
@@ -2,6 +2,11 @@ export type ISyncStatus = {
 	connectedPeers: string[];
 };
 
+// type MultiLevelSync = {
+// 	before: number | undefined;
+// 	after: number | undefined;
+// };
+
 export type Synchronization = {
 	syncValue: number | undefined;
 	shouldSync: boolean;
